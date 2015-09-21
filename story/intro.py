907181913df407b279e1612a155e6c320a6f6e3f
Tyler Hebner <smokiemario@github.com>
2015-09-21 15:44:43 -0400

    save

--- story/intro.py
+++ story/intro.py
@@ -2,6 +2,9 @@
 
 You are in a field and see a lot of tall grass. You also see a couple of
 houses. There are a couple of trees but not too many. It is very hot so
-you go find a tree to get some shade.
+you go find a tree to get some shade. You may either rest under the tree
+or explore.
 """
 
+on_rest="undertree"
+on_explore="explore"
--- story/undertree.py
+++ story/undertree.py
@@ -4,3 +4,7 @@ Under the tree you notice something running
 through the grass. You go to investigate.
 Suddenly, A wild leopard attacks! What do you do?
 """
+
+def action(s):
+    print("""(Would do a battle)""")
+    s.pause()
